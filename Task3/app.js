603d6332a0ac6487a4b4ba7b51161b18306b364d
inryceu <pavel.maluev@gmail.com>
2026-03-23 23:30:51 +0200

    implemented all for lab3

--- Task3/app.js
+++ Task3/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import programRoutes from "./src/routes/ProgramRoutes.js";
+import channelRouter from "./src/routes/ChannelRoutes.js";
+import showRouter from "./src/routes/ShowRoutes.js";
 import authRouter from "./src/routes/AuthRoutes.js";
 import cookieParser from "cookie-parser";
 
@@ -19,9 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/schedule", programRouter);
+app.use("/schedule", programRoutes);
 app.use("/auth", authRouter);
 app.use("/", showRouter);
+app.use("/", channelRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
